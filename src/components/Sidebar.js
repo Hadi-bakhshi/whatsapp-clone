402f3412961b6ea2a4bfa89d28f4b22cb4d1d3eb
hadi <hadibakhshi277@gmail.com>
2022-03-16 15:42:10 +0330

    search and addRoom btn added to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,48 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { ExitToApp } from "@material-ui/icons";
+import { Add, ExitToApp, SearchOutlined } from "@material-ui/icons";
 import React from "react";
-import {auth} from '../firebase'
+import { auth } from "../firebase";
 import "./Sidebar.css";
 
-export default function Sidebar({user,page}) {
-
-  function signOut(){
+export default function Sidebar({ user, page }) {
+  function signOut() {
     auth.signOut();
   }
 
-
-  return <div className="sidebar" style={{
-    minHeight: page.isMobile ? page.height : "auto"
-  }}>
-    <div className="sidebar__header">
-      <div className="sidebar__header--left">
-        <Avatar src={user?.photoURL} />
-        <h4>{user?.displayName}</h4>
+  return (
+    <div
+      className="sidebar"
+      style={{
+        minHeight: page.isMobile ? page.height : "auto",
+      }}
+    >
+      <div className="sidebar__header">
+        <div className="sidebar__header--left">
+          <Avatar src={user?.photoURL} />
+          <h4>{user?.displayName}</h4>
+        </div>
+        <div className="sidebar__header--right">
+          <IconButton onClick={signOut}>
+            <ExitToApp />
+          </IconButton>
+        </div>
       </div>
-      <div className="sidebar__header--right">
-        <IconButton onClick={signOut}>
-          <ExitToApp/>
+      <div className="sidebar__search">
+        <form className="sidebar__search--container">
+          <SearchOutlined />
+          <input
+            placeholder="Search for users or rooms"
+            type="text"
+            id="search"
+          />
+        </form>
+      </div>
+      <div className="sidebar__chat--addRoom">
+        <IconButton>
+          <Add />
         </IconButton>
       </div>
     </div>
-  </div>;
+  );
 }
+ 
